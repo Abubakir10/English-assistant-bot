f1341504688e50948bd3c720d4de335e440c022b
Asadlev <asadlev.org@gmail.com>
2025-06-24 14:41:06 +0600

    added loginc - send image

--- handlers/bot_handler.py
+++ handlers/bot_handler.py
@@ -1,12 +1,17 @@
 from aiogram import types
 from handlers import texts, keyboards
+from aiogram.types import FSInputFile
+
 
 async def start(message: types.Message):
-    await message.answer(
-        text=texts.start_tx,
+    my_phono = FSInputFile("images/python-book.jpg")
+    await message.answer_photo(
+        photo=my_phono,
+        caption=texts.start_tx,
         reply_markup=keyboards.start_kb(),
-        parse_mode='HTML'
+        parse_mode="HTML"
     )
+
 
 
 
--- main.py
+++ main.py
@@ -16,14 +16,6 @@ dp = Dispatcher()
 
 logger = logging.getLogger(__name__)
 
-async def on_startup():
-    """Функция, выполняемая при запуске бота"""
-    logger.info("Бот успешно запущен")
-
-async def on_shutdown():
-    """Функция, выполняемая при выключении бота"""
-    logger.info("Бот выключается...")
-
 
 async def register_handlers(dp: Dispatcher):
     dp.message.register(bot_handler.start, Command('start'))
@@ -33,14 +25,8 @@ async def main():
     # Регистрация обработчиков
     await register_handlers(dp)
 
-    # Установка функций запуска/остановки
-    dp.startup.register(on_startup)
-    dp.shutdown.register(on_shutdown)
-
     logger.info("Запуск бота...")
     await dp.start_polling(bot)
-
-
 
 if __name__ == '__main__':
     asyncio.run(main())
